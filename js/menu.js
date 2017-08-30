00ba82d973c4901d1aca399bd6acf53708b14b8a
mrpsb <mrpsb@hotmail.com>
2017-08-30 22:54:51 +0100

    Poo

--- js/menu.js
+++ js/menu.js
@@ -3,13 +3,14 @@
 const menuState = {
   
   create: function () {
-      const nameLabel = game.add.text(80, 80, 'My First Game', {font: '50px Arial', fill: '#ffffff'});
       
-      const startLabel = game.add.text(80,game.world.height-80,"press the W key to start", {font: '25px Arial', fill: '#ffffff'});
+    const nameLabel = game.add.text(80, 80, 'Arseteroids', {font: '50px Arial', fill: '#ffffff'});
+     
+    const startLabel = game.add.text(80,game.world.height-80,"press the W key to start", {font: '25px Arial', fill: '#ffffff'});
 
-      const wkey = game.input.keyboard.addKey(Phaser.Keyboard.W);
+    const wkey = game.input.keyboard.addKey(Phaser.Keyboard.W);
 
-      wkey.onDown.addOnce(this.start, this);
+    wkey.onDown.addOnce(this.start, this);
   },
 
   start: function () {
